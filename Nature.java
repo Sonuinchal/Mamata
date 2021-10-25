215a7f3bf26b2f1874eb2df1567bbfd255239192
Sonuinchal <93113348+Sonuinchal@users.noreply.github.com>
2021-10-25 23:25:34 +0530

    methods

---
+++ Nature.java
@@ -0,0 +1,111 @@
+class Nature {
+	
+	static String playfair(){
+		System.out.println("invoke playfair()");
+		int olympicgames = 134;
+		if(olympicgames == 134) {
+		//System.out.println(" lots of games");
+		return "lots of games";
+	}
+	else {
+		String res= "lots of not games";
+		return res;
+	}
+	}
+
+	static String rainy() {
+		System.out.println("invoke rainy()");
+		byte cloud = 8;
+		if(cloud == 8) {
+		//System.out.println(" Todays is cloudy");
+		return "Todays is cloudy";
+		}
+		else {
+			String res= "Todays is cloudy";
+			return res;
+	}
+	}
+	static String colorfull() {
+		System.out.println("invoke colorfull()");
+		short sunrise = 2;
+		if(sunrise == 2){
+       //System.out.println("sunrise shows shiny");
+		return "sunrise shows shiny";
+		}
+		else {
+			String res = "sunrise shows shiny";
+			return res;
+      
+		}
+	}
+	static String glow() {
+		System.out.println("invoke glow()");
+		long stars = 50000l;
+		if(stars == 50000) {
+		//System.out.println("Bunch of stars");
+		return "Bunch of stars";
+		}
+		else {
+			String res = "Bunch of stars";
+			return res;
+      
+		}
+	}
+    static String aim() {
+		System.out.println("invoke aim()");
+		char dream = 'd';
+		if(dream == 'd') {
+		//System.out.println("we have lots of dreams");
+		return "we have lots of dreams";
+		}
+		else {
+		
+		String res = "we have lots of dreams";
+		return res;
+		}
+	}
+	static String farmer() {
+	System.out.println("invoke farmer()");		
+		float agricluture = 2.0f;
+		if(agricluture == 2.0) {
+		//System.out.println(" Agricluture is backbone of our country");
+		return "Agricluture is backbone of our country";
+		}
+		else {
+		
+		String res = "Agricluture is backbone of our country";
+		return res;
+		}
+	}
+	
+	static String powder() {
+		System.out.println("invoke powder()");
+		double makeupkit = 6.0d;
+		if(makeupkit == 6.0) {
+		//System.out.println("A smile is the best makeup any girl can wear");
+		return "A smile is the best makeup any girl can wear";
+		}
+		else {
+		
+		String res = "A smile is the best makeup any girl can wear";
+		return res;
+		}
+	}
+	
+	static String pixels() {
+		System.out.println("invoke pixels()");
+		boolean camera = true;
+		if(camera == true) {
+		//System.out.println("Its CapturesPictures");
+		return "Its CapturesPictures";
+		}
+		else {
+		
+		String res = "Its CapturesPictures";
+		return res;
+		}
+	}
+	
+}
+		
+	
---
+++ Sateri.java
@@ -0,0 +1,12 @@
+class Hillstation{
+	static void sunrise(){
+		System.out.println("Beautiful place with great scenery around.");
+	}
+}
+
+public class Sateri{
+	public static void main(String[] args){
+		Hillstation.sunrise();
+		System.out.println("Masoon is the best season to visit it.");
+	}
+}
